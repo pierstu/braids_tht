26a34d0da959b6a7b276a241c41eeff0fd10b5fb
pierstu <pierre.studievic@protonmail.com>
2020-06-28 10:01:19 +0200

    bees/braids
    Braids firmware Bees in the trees

---
+++ bees/braids/macro_oscillator.d
@@ -0,0 +1,5 @@
+build/braids/macro_oscillator.o: braids/macro_oscillator.cc \
+ braids/macro_oscillator.h stmlib/stmlib.h braids/analog_oscillator.h \
+ braids/resources.h braids/digital_oscillator.h braids/excitation.h \
+ braids/svf.h stmlib/utils/dsp.h braids/settings.h \
+ braids/parameter_interpolation.h
